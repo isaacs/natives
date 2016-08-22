3dda5309d19cef6f0781f6e0a05be2e6f2115510
isaacs <i@izs.me>
2016-08-22 11:54:05 -0700

    add a test to reproduce the buffer weirdness

--- test/basic.js
+++ test/basic.js
@@ -1,5 +1,33 @@
-var t = require('tap')
 var natives = require('../')
+var buf = natives.require('buffer')
+
+var t = require('tap')
+t.test('it doesnt break buffers and streams', function (t) {
+  var code = [
+    'require(process.argv[1]).require("buffer")',
+    'process.stdin.pipe(process.stdout)'
+  ].join('\n')
+
+  var spawn = require('child_process').spawn
+  var node = process.execPath
+  var args = ['-e', code, require.resolve('../')]
+  var opts = {
+    stdio: [ 'pipe', 'pipe', 2 ]
+  }
+  var child = spawn(node, args, opts)
+  var out = ''
+  child.stdout.on('data', function (c) {
+    out += c
+  })
+  child.stdin.write('ok\n')
+  child.stdin.end()
+  child.on('close', function (exit, signal) {
+    t.notOk(exit, 'exit success')
+    t.notOk(signal, 'no signal')
+    t.equal(out, 'ok\n')
+    t.end()
+  })
+})
 
 t.test('fs is like fs but not fs', function (t) {
   var fs = require('fs')
@@ -11,7 +39,7 @@ t.test('fs is like fs but not fs', function (t) {
   t.end()
 })
 
-t.test('fall back to normal require', function (t) {
+t.test('handle not-found modules with grace', function (t) {
   t.equal(natives.require('./not-a-node-internal/.'), undefined)
   t.end()
 })
